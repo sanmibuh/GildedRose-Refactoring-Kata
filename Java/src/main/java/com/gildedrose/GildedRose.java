bee5a452992067c8c7d9c4cc711fd468209c3959
David Belmez Vicente <sanmibuh@gmail.com>
2021-10-19 08:03:40 +0200

    [main]: GlidedRose refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,8 +13,8 @@ class GildedRose {
   }
 
   public void updateQuality() {
-    for (int i = 0; i < items.length; i++) {
-      items[i].updateQuality();
+    for (final Item item : items) {
+      item.updateQuality();
     }
   }
 }
