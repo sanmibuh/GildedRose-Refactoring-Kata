dbda78fb4e8ba8e0dc62be5991139c49a1734562
David Belmez Vicente <sanmibuh@gmail.com>
2021-10-19 08:16:54 +0200

    [main]: Regular item refactor

--- Java/src/main/java/com/gildedrose/RegularItem.java
+++ Java/src/main/java/com/gildedrose/RegularItem.java
@@ -8,14 +8,13 @@ public class RegularItem extends Item {
 
   @Override
   public void updateQuality() {
-    if (quality > 0) {
-      quality -= 1;
-    }
-    sellIn -= 1;
+    sellIn--;
 
-    if (sellIn < 0) {
-      if (quality > 0) {
-        quality -= 1;
+    if (quality > 0) {
+      quality--;
+
+      if (sellIn < 0 && quality > 0) {
+        quality--;
       }
     }
   }
